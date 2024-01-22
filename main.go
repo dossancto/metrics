0f58b9c9aeb0b96dcbcb408f3ffdba57e54e74b0
lu-css <dossantossilvalucas963@gmail.com>
2024-01-22 11:12:21 -0300

    [chore] create project

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+
+	docs "github.com/lu-css/metrics/docs"
+
+	metrics "github.com/lu-css/metrics/src/ui/controllers/metrics"
+)
+
+func main() {
+	r := gin.Default()
+
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/example")
+		{
+			eg.GET("/helloworld", metrics.Index)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "eae pogao")
+	})
+
+	r.Run()
+}
---
+++ src/ui/controllers/metrics/metrics_controller.go
@@ -0,0 +1,22 @@
+package metrics
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
+func Index(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"users": "hello world"})
+}
